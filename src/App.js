84a05a27d60180ac60319bb41e6ef359095d9dd0
Muhammed ÖZDOĞAN <muhammed.ozdogan2@ogr.sakarya.edu.tr>
2020-02-17 23:11:42 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import Header from './component/header/Header';
+import Footer from './component/Footer';
+import About from './component/About';
+import Resume from './component/Resume';
+import Contact from './component/Contact';
+import Testimonials from './component/Testimonials';
+import Portfolio from './component/Portfolio';
 
 class App extends Component {
 
