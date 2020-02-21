46b511f895b10df500a9f0aff1dc0ec5e6cf7af4
Muhammed ÖZDOĞAN <muhammed.ozdogan2@ogr.sakarya.edu.tr>
2020-02-21 16:21:44 +0300

    Docker file has been added

---
+++ build/precache-manifest.57428e979accd7929279c8567e0e8dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bca7ea3b53a2c98eba7c86fb222f144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a68469a04eaf24be1e7",
+    "url": "/static/css/main.c26386fa.chunk.css"
+  },
+  {
+    "revision": "a0c48c0a705e5465eead",
+    "url": "/static/js/2.ba90465d.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.ba90465d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a68469a04eaf24be1e7",
+    "url": "/static/js/main.d82748f6.chunk.js"
+  },
+  {
+    "revision": "d0fe746fe6ad2bdca125",
+    "url": "/static/js/runtime-main.5a4af871.js"
+  }
+]);
